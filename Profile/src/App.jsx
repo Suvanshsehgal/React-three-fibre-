0a4c0103194cedc47d3bc4b24c12f83cc8c6ba7f
Suvanshsehgal <suvanshsehgal@gmail.com>
2025-08-28 22:49:46 +0530

    Added assets

--- Profile/src/App.jsx
+++ Profile/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
 
   return (
     <>
-     <div className='text-red-500'>Hello worlf </div>
+     <div className='text-red-500'>Hello world </div>
     </>
   )
 }
